78de6bd097c0780a4b62062ba120c605de77a2e4
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-06-09 14:54:45 -0400

    17k test

--- openidl-extraction-pattern-developer/data-loader.js
+++ openidl-extraction-pattern-developer/data-loader.js
@@ -2,7 +2,7 @@ const MongoDBManager = require('./service/mongo-database-manager')
 const conn = require('./connection.json')
 const dbName = conn.dbName
 const collectionName = conn.collectionName
-const testData2 = require('../openidl-project-gamma/data/processed/idmPremiumRecords.json').records
+const testData2 = require('../../con-data/auto.json').records
 //const testData = require('./test/data/trivialSampleData.json').records
 //console.log(testData);
 
@@ -12,7 +12,7 @@ async function loadData(dbManager, dbName, collectionName, data) {
     await dbManager.connect()
     await dbManager.useDatabase(dbName).catch((err) => { throw err })
     await dbManager.dropCollection(collectionName).catch((err) => { throw err })
-    await dbManager.loadData(testData2, dbName, collectionName)
+    await dbManager.loadData(data, dbName, collectionName)
 }
 
 var manager = new MongoDBManager({ url: "mongodb://localhost:27017" })
