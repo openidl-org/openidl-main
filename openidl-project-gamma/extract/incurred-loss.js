022499695e9b2d57d26fcc277e7b45e9d0c92233
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-08-11 17:45:10 -0400

    format_doc

--- openidl-project-gamma/extract/incurred-loss.js
+++ openidl-project-gamma/extract/incurred-loss.js
@@ -6,88 +6,110 @@ const util = require('util');
 var help = require('../transform/helper');
 const dbName = conn.dbName;
 
-
 async function find(collection, query) {
 	let records = await manager.getRecords(dbName, collection, query);
 	//console.log('records length: ' + records.length);
 	return records;
 }
 
-
-function sumPaidLoss(records){
-    let loss = 0
-    //sum up all records
-    for (let record of records){
-        loss+=record.Claim.LossAmount
-    }
-    loss.toFixed(2)
-    return loss
+function sumPaidLoss(records) {
+	let loss = 0;
+	//sum up all records
+	for (let record of records) {
+		loss += record.Claim.LossAmount;
+	}
+	loss.toFixed(2);
+	return loss;
 }
 
-function sumOutstanding(records){
-    console.log('sum outstand: '+records.length)
-    
-    //object to hold the records
-    resultSet ={}
-    for (let record of records){
-        //console.log('identifier: '+record.Claim.OccurrenceIdentifier)
-        
-        //check if OccurrenceIdentifierw already exists in result set
-        let present = null
-        if (record.Claim.OccurrenceIdentifier in resultSet){
-            //console.log('True')
-            present = true
-        }else {
-           // console.log('False')
-            present = false
-        }
+function sumOutstanding(records) {
+	console.log('sum outstand: ' + records.length);
 
-        if (!present){
-            //new OccurrenceIdentifier, add to result set
-            resultSet[record.Claim.OccurrenceIdentifier] = {"accountingDate": record.Policy.AccountingDate, "loss": record.Claim.LossAmount}
-        }else{
-            //OccurrenceIdentifier already exists, check to make sure we have the most recent version
-            if (record.Policy.AccountingDate > resultSet[record.Claim.OccurrenceIdentifier]){
-                resultSet[record.Claim.OccurrenceIdentifier] = {"accountingDate": record.Policy.AccountingDate, "loss": record.Claim.LossAmount}
-            }
-        }
-    }
-    let total = 0
-    
-    //sum the most recent loss records
-    for (let result in resultSet){
-        let localLoss = resultSet[result].loss
-        total+=localLoss
-    }
-    return total
+	//object to hold the records
+	resultSet = {};
+	for (let record of records) {
+		//console.log('identifier: '+record.Claim.OccurrenceIdentifier)
+
+		//check if OccurrenceIdentifierw already exists in result set
+		let present = null;
+		if (record.Claim.OccurrenceIdentifier in resultSet) {
+			//console.log('True')
+			present = true;
+		} else {
+			// console.log('False')
+			present = false;
+		}
+
+		if (!present) {
+			//new OccurrenceIdentifier, add to result set
+			resultSet[record.Claim.OccurrenceIdentifier] = {
+				accountingDate: record.Policy.AccountingDate,
+				loss: record.Claim.LossAmount
+			};
+		} else {
+			//OccurrenceIdentifier already exists, check to make sure we have the most recent version
+			if (
+				record.Policy.AccountingDate >
+				resultSet[record.Claim.OccurrenceIdentifier]
+			) {
+				resultSet[record.Claim.OccurrenceIdentifier] = {
+					accountingDate: record.Policy.AccountingDate,
+					loss: record.Claim.LossAmount
+				};
+			}
+		}
+	}
+	let total = 0;
+
+	//sum the most recent loss records
+	for (let result in resultSet) {
+		let localLoss = resultSet[result].loss;
+		total += localLoss;
+	}
+	return total;
 }
 
-async function getIncurredLoss(start,end,coverageCode){
-    await manager.connect();
-    
-    //get all paid loss within time line
-    let q1 = {$and: [{"TransactionCode": "2"},{"Claim.AccidentDate": {$gte: start}},{"Claim.AccidentDate": {$lte: end}}]}
-    
-    //get all outstanding loss within time limits
-    let q2 = {$and: [{"TransactionCode": "3"},{"Claim.AccidentDate": {$gte: start}},{"Claim.AccidentDate": {$lte: end}}]}
+async function getIncurredLoss(start, end, coverageCode) {
+	await manager.connect();
 
-    let lossRecords = await find('insurance',q1)
-    let outstandingRecords = await find('insurance',q2)
-    await manager.disconnect();
+	//get all paid loss within time line
+	let q1 = {
+		$and: [
+			{ TransactionCode: '2' },
+			{ 'Claim.AccidentDate': { $gte: start } },
+			{ 'Claim.AccidentDate': { $lte: end } }
+		]
+	};
 
+	//get all outstanding loss within time limits
+	let q2 = {
+		$and: [
+			{ TransactionCode: '3' },
+			{ 'Claim.AccidentDate': { $gte: start } },
+			{ 'Claim.AccidentDate': { $lte: end } }
+		]
+	};
 
-    let paidLoss = sumPaidLoss(lossRecords)
-    let outstandLoss = sumOutstanding(outstandingRecords)
-    let incurredLoss = paidLoss+outstandLoss
+	let lossRecords = await find('insurance', q1);
+	let outstandingRecords = await find('insurance', q2);
+	await manager.disconnect();
 
-    console.log('\n\n\n\n   Paid Loss: '+paidLoss+'\n   Outstanding Loss: '+ outstandLoss+'\n   Incurred Loss:'+ incurredLoss)
+	let paidLoss = sumPaidLoss(lossRecords);
+	let outstandLoss = sumOutstanding(outstandingRecords);
+	let incurredLoss = paidLoss + outstandLoss;
+
+	console.log(
+		'\n\n\n\n   Paid Loss: ' +
+			paidLoss +
+			'\n   Outstanding Loss: ' +
+			outstandLoss +
+			'\n   Incurred Loss:' +
+			incurredLoss
+	);
 }
 
+let start = '2020-02-01';
+let end = '2021-01-01';
+let coverageCode = 1;
 
-let start = "2020-02-01"
-let end = "2021-01-01"
-let coverageCode = 1
-
-
-
-getIncurredLoss(start,end,coverageCode)
+getIncurredLoss(start, end, coverageCode);
