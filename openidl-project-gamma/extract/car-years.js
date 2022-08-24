7f80cf0be2dda1b3b56a89e0773494f507a685bf
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-08-24 12:21:59 -0400

    aug24-apk

--- openidl-project-gamma/extract/car-years.js
+++ openidl-project-gamma/extract/car-years.js
@@ -13,38 +13,43 @@ async function find(collection, query) {
 }
 
 function carYear1(coverageCode, records){
-    for (let record of records){
-		for (let coverage of record.Coverages[coverageCode].CoverageRecords){
-			//console.table(coverage)
-			earnedPremium+=coverage.PremiumAmount
-		}
-	}
+    //sum months covered
+
+    group by policy
 }
 
 async function getCarYears(start, end, coverageCode){
     await manager.connect();
 
-    let q1 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$lt: start}},
-	{"Coverages.1.CoverageRecords.AccountingTermExpiration": {$lt:end }}]}
+    let q1 = {$and: [{"Policy.AccountingDate": {$lt: start}},
+	{"Policy.AccountingTermExpiration": {$lt:end }},{"Coverage.CoverageCode": coverageCode}]}
     
-	let q2 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$lt: start}},
-    {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$lte:end }},
-    {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gte:start}}]}
+	// let q2 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$lt: start}},
+    // {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$lte:end }},
+    // {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gte:start}}]}
 
-    let q3 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$gte: start}},
-	{"Coverages.1.CoverageRecords.AccountingDate": {$lte: end}},
-	{"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gt:end }}]}
+    // let q3 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$gte: start}},
+	// {"Coverages.1.CoverageRecords.AccountingDate": {$lte: end}},
+	// {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gt:end }}]}
 
-    let q4 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$lte: start}},
-    {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gt:end }}]}
+    // let q4 = {$and: [{"Coverages.1.CoverageRecords.AccountingDate": {$lte: start}},
+    // {"Coverages.1.CoverageRecords.AccountingTermExpiration": {$gt:end }}]}
 
 
     //records
     console.log('Find Group One')
     let r1 = await find('policy',q1)
+    console.log(r1.length)
 
     await manager.disconnect();
+    return r1
 
 }
+
+let start = '2020-02-01';
+let end = '2021-01-01';
+let coverageCode = "1";  
+let carYears = getCarYears(start,end,coverageCode)
+
 
 module.exports = {getCarYears}
