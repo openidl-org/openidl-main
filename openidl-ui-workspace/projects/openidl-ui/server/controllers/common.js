f78285e3ce75cc85321bc8b74452ce21e111225b
T470P-09\surendra.sirangu <surendra.sirangu@itpeoplecorp.com>
2022-09-26 15:34:12 +0530

    commit Jurisdiction Icon

--- openidl-ui-workspace/projects/openidl-ui/server/controllers/common.js
+++ openidl-ui-workspace/projects/openidl-ui/server/controllers/common.js
@@ -54,6 +54,8 @@ common.getSearchDataCalls = (req, res) => {
 
 common.handleRequest = (req, res, url) => {
     logger.debug('Inside handle request');
+    logger.debug("url :", url);
+    logger.debug("DATA_CALL_APP_URL :", process.env.DATA_CALL_APP_URL + url);
     if (!common.ifUrlSafe(url)) {
         logger.debug('request url did not pass safety check.');
         const jsonRes = {
@@ -161,6 +163,21 @@ common.getDataCalls = (req, res) => {
     } else {
         common.rejectRequest(req, res, url);
     }
+}
+
+common.getIconBucketUrl = (req, res) => {
+    var url = '/icon-bucket-url';
+
+    logger.debug('Environment Icon Bucket');
+    logger.debug("url :", url);
+    logger.debug("ICON_BUCKET_URL: ", process.env.ICON_BUCKET_URL);
+
+    let jsonRes = {
+        statusCode: 200,
+        success: true,
+        result: process.env.ICON_BUCKET_URL
+    };
+    util.sendResponse(res, jsonRes);
 }
 
 common.getLOBs = (req, res) => {
