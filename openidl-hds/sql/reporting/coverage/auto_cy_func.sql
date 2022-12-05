6513b3a4d6f17f1437d1a9011abc53c1d3ec992e
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-12-05 14:29:50 -0500

    refactor-for-accessibility

---
+++ openidl-hds/sql/reporting/coverage/auto_cy_func.sql
@@ -0,0 +1,44 @@
+drop function auto_cy;    
+CREATE OR replace FUNCTION auto_cy(IN start_date date,IN end_date date, IN pv_coverage_code VARCHAR)
+returns      numeric AS $$DECLARE cy numeric;
+BEGIN
+    select sum(a.cy)
+    from (
+        (SELECT 1 ggroup,            
+            Datediff(accounting_term_expiration, accounting_date)/12*exposure cy
+            FROM   openidl_base.au_premium
+            WHERE  accounting_date >= start_date
+            AND accounting_term_expiration < end_date
+            and transaction_code = '1'
+            and coverage_code = pv_coverage_code) 
+        union all
+            (SELECT 2 ggroup,                                                                
+            Datediff(accounting_term_expiration, start_date)/12*exposure cy
+            FROM   openidl_base.au_premium
+            WHERE  accounting_date < start_date
+            and accounting_term_expiration> start_date
+            and accounting_term_expiration< end_date
+            and transaction_code = '1'
+            and coverage_code = pv_coverage_code)
+        union all
+            (select 3 ggroup,
+            Datediff(accounting_date, end_date)/12*exposure cy
+            FROM   openidl_base.au_premium
+            WHERE  accounting_date > start_date
+            and accounting_date < end_date
+            AND accounting_term_expiration > end_date
+            and transaction_code = '1'
+            and coverage_code = pv_coverage_code)
+        union all
+            (select 4 ggroup,       
+            Datediff(end_date, start_date)/12*exposure cy
+            FROM   openidl_base.au_premium
+            WHERE  accounting_date < start_date
+            AND accounting_term_expiration > end_date
+            and transaction_code = '1'
+            and coverage_code = pv_coverage_code)
+            ) a into cy;
+    RETURN cy;
+END$$ language plpgsql
+
+select auto_cy('2000-01-01'::DATE, '2000-02-01'::DATE);
