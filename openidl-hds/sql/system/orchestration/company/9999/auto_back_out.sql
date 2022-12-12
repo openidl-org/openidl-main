c7d25798b8cd1a0ae0d4ef6a1241c51d0be42735
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2022-12-12 12:41:21 -0500

    fixed ep

--- openidl-hds/sql/system/orchestration/company/9999/auto_back_out.sql
+++ openidl-hds/sql/system/orchestration/company/9999/auto_back_out.sql
@@ -3,15 +3,17 @@
     DROP TABLE IF EXISTS openidl_base_9999.au_premium;
     DROP TABLE IF EXISTS openidl_base_9999.au_loss;
 
-    DELETE FROM IF EXISTS openidl_base_9999.au_premium;
-    DELETE FROM IF EXISTS TABLE openidl_base_9999.au_loss;
+    DELETE FROM  openidl_base_9999.au_premium;
+    DELETE FROM  TABLE openidl_base_9999.au_loss;
 
     -- Temp Tables
-    DROP TABLE IF EXISTS openidl_ep_9999.tmp_pre_au_coverage;
-    DROP TABLE IF EXISTS openidl_ep_9999.tmp_au_coverage;
+    DROP TABLE  openidl_ep_9999.tmp_au_coverage_ref;
+    DROP TABLE  openidl_ep_9999.tmp_au_coverage;
+    DROP TABLE  openidl_ep_9999.tmp_pre_au_coverage;
 
-    DELETE FROM IF EXISTS openidl_ep_9999.tmp_pre_au_coverage;
-    DELETE FROM IF EXISTS openidl_ep_9999.tmp_au_coverage;
+    DELETE FROM  openidl_ep_9999.tmp_pre_au_coverage;
+    DELETE FROM  openidl_ep_9999.tmp_au_coverage;
+    
 
     --Functions (note should be torn down after each extraction);
     DROP FUNCTION IF EXISTS openidl_ep_9999.tmp_car_years;
