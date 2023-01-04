5028cea7bac6532abc7fb2efabde27cccecc0f2d
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2023-01-04 09:24:59 -0500

    break-out-map

--- openidl-hds/sql/system/orchestration/ref/function/auto/auto_build_extraction.js
+++ openidl-hds/sql/system/orchestration/ref/function/auto/auto_build_extraction.js
@@ -1,14 +1,5 @@
 const getCoverageReport = require('./auto_coverage_report');
-const getCarYears = require('./auto_cy_func');
-const getEarnedPremium = require('./auto_ep_func')
-const getIncurredCount = require('./auto_ic_func')
-const getIncurredLoss = require('./auto_il_func')
-const getPreTable = require('./auto_tmp_pc.js')
-const getReportingTable = require('./auto_tmp_reporting_tbl.js')
-const getTearDown = require('./auto_tear_down.js')
-const getAutoOutstanding = require('./auto_outstanding.js')
-const getRefTable = require('./auto_tmp_report_ref_tbl.js')
-
+const getMap = require('./auto_coverage_map')
 
 function getBuilder(companyId){
 
@@ -19,23 +10,6 @@ function getBuilder(companyId){
     extractionPattern = {"map": map, "reduce": reduce, "clean": clean}
     return extractionPattern
 }
-
-function getMap(companyId){
-    sqlArray = []
-    sqlArray.push(getTearDown(companyId))
-    sqlArray.push(getPreTable(companyId))
-    sqlArray.push(getReportingTable(companyId))
-    sqlArray.push(getRefTable(companyId))
-    sqlArray.push(getCarYears(companyId))
-    sqlArray.push(getEarnedPremium(companyId))
-    sqlArray.push(getAutoOutstanding(companyId))
-    sqlArray.push(getIncurredLoss(companyId))
-    sqlArray.push(getIncurredCount(companyId))
-    //sqlArray.push(getCoverageReport(companyId))
-    sqlString = sqlArray.join(' ') //join on what? 
-    return sqlString    
-}
-
 
 
 module.exports = getBuilder
--- openidl-hds/sql/system/orchestration/ref/function/auto/auto_coverage_report_sql.js
+++ openidl-hds/sql/system/orchestration/ref/function/auto/auto_coverage_report_sql.js
@@ -1,31 +1,7 @@
 const getCoverageReport = require('./auto_coverage_report');
-const getCarYears = require('./auto_cy_func');
-const getEarnedPremium = require('./auto_ep_func')
-const getIncurredCount = require('./auto_ic_func')
-const getIncurredLoss = require('./auto_il_func')
-const getPreTable = require('./auto_tmp_pc.js')
-const getReportingTable = require('./auto_tmp_reporting_tbl.js')
-const getTearDown = require('./auto_tear_down.js')
-const getAutoOutstanding = require('./auto_outstanding.js')
-const getRefTable = require('./auto_tmp_report_ref_tbl.js')
+const getMap = require('./auto_coverage_map')
 
 
-
-function getMap(companyId){
-    sqlArray = []
-    sqlArray.push(getTearDown(companyId))
-    sqlArray.push(getPreTable(companyId))
-    sqlArray.push(getReportingTable(companyId))
-    sqlArray.push(getRefTable(companyId))
-    sqlArray.push(getCarYears(companyId))
-    sqlArray.push(getEarnedPremium(companyId))
-    sqlArray.push(getAutoOutstanding(companyId))
-    sqlArray.push(getIncurredLoss(companyId))
-    sqlArray.push(getIncurredCount(companyId))
-    //sqlArray.push(getCoverageReport(companyId))
-    sqlString = sqlArray.join(' ') //join on what? 
-    return sqlString    
-}
 
 function getBuilder(companyId){
 
