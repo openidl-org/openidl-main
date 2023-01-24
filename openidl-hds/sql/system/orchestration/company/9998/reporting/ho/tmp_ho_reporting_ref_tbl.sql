e7c8202193ecfc808b863c5f5bc54c28ea5a5c8a
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2023-01-24 09:59:40 -0500

    delete-company

--- openidl-hds/sql/system/orchestration/company/9998/reporting/ho/tmp_ho_reporting_ref_tbl.sql
+++
@@ -1,6 +0,0 @@
-
-    CREATE TABLE openidl_ep_9998.tmp_ho_reporting_ref as
-    SELECT reporting_category, reporting_form, policy_form
-    FROM openidl_ep_9998.tmp_ho_pc
-    GROUP BY reporting_category, reporting_form, policy_form;
-    
