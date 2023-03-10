848cecb5c6ea1ae33d1d7dc439439162dfe1bdb7
Peter Antley <peterantley@Amers-MBP.attlocal.net>
2023-03-10 10:06:14 -0500

    rm fk id from brake

--- openidl-hds-ddl/ref/pa/tables/pa_anti_lock_brakes_discount_code.sql
+++ openidl-hds-ddl/ref/pa/tables/pa_anti_lock_brakes_discount_code.sql
@@ -1,16 +1,15 @@
-do $$ 
-begin
-create table if not exists pa_anti_lock_brakes_discount_code (
-    id int,
-    fk_state_id int,
-    code varchar,
-    description varchar
+DO $$ 
+BEGIN
+CREATE TABLE IF NOT EXISTS pa_anti_lock_brakes_discount_code (
+    id INT,
+    code VARCHAR,
+    description VARCHAR
 );
 
-if not exists (select * from pa_anti_lock_brakes_discount_code) then 
-insert into pa_anti_lock_brakes_discount_code (id, code, description) values( 1,'1','Anti-Lock Braking System discount applied to this coverage.' );
-insert into pa_anti_lock_brakes_discount_code values( 2,'2','Program offers an Anti-Lock Braking System discount on this coverage, but the vehicle does not qualify for the discount.' );
-insert into pa_anti_lock_brakes_discount_code values( 3,'9','Not Applicable -Program does not offer an Anti-Lock Braking System discount on this coverage.' );
-end if;
+IF NOT EXISTS (SELECT * FROM pa_anti_lock_brakes_discount_code) THEN 
+INSERT INTO pa_anti_lock_brakes_discount_code (id, code, description) VALUES( 1,'1','Anti-Lock Braking System discount applied to this coverage.' );
+INSERT INTO pa_anti_lock_brakes_discount_code VALUES( 2,'2','Program offers an Anti-Lock Braking System discount on this coverage, but the vehicle does not qualify for the discount.' );
+INSERT INTO pa_anti_lock_brakes_discount_code VALUES( 3,'9','Not Applicable - Program does not offer an Anti-Lock Braking System discount on this coverage.' );
+END IF;
 
-end $$
+END $$;
