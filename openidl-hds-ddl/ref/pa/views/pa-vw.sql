ebd96a7cef9fd95a0d36805a3402abca61326c70
antleypk2 <97132380+antleypk2@users.noreply.github.com>
2023-03-10 18:06:54 -0500

    pa-vw

--- openidl-hds-ddl/ref/pa/views/pa-vw.sql
+++ openidl-hds-ddl/ref/pa/views/pa-vw.sql
@@ -1,10 +1,10 @@
-create view 
+set schema 'openidl_base_9999';
 SELECT a.id openidl_id,
        a.policy_num,
        a.claim_id,
        a.zip,
        a.zip_suff,
-	   a.rep_mo monthh,
+	   a.rep_mo accounting_month,
 	   a.comp company,
 	   a.territory territory,
 	   a.prem_amt  premium_amount,
@@ -18,8 +18,10 @@ SELECT a.id openidl_id,
 	   		when a.accident_yr::numeric > 90 then concat('19',a.accident_yr)
 			else concat('20',a.accident_yr)
 		end accident_year,
-	   concat(substring((date_part('year', CURRENT_DATE)::varchar),1,3),a.rep_yr) yearr, 
-       a.id   openidl_id,
+		case 
+	   		when a.rep_yr is null then null
+			else concat(substring((date_part('year', CURRENT_DATE)::varchar),1,3),a.rep_yr)
+		end accounting_year,   
 	   b.id   fk_openidl_lob_code_id,
        c.id   fk_subline_code_id,
        d.id   fk_state_code_id,
@@ -35,50 +37,25 @@ SELECT a.id openidl_id,
        q.id fk_package_code,
        r.id fk_passive_restraint_discount_code,
        s.id fk_anti_lock_brakes_discount_code,
-       t.id fk_defensive_driver_discount_code,
-       a.lia_lim liability_limit_code,
-       a.driv_train_good_student driver_train_good_student_code,
-       a.ded_amt deductible_amount_code,
-       a.col cause_of_loss_code,
-       a.um_uim_motorist um_uim_motorist_code,
-       a.um_uim_stack_ind um_uim_stack_ind_code,
-       a.sing_mult_car sing_mult_car_code,
-       a.symbol symbol_code,
-       a.anti_theft  anti_theft_code
-FROM   pa_stat_stg a,
-       openidl_lob_code b,
-       pa_subline_code c,
-       state_code d,
-       pa_transaction_code e,
-       pa_program_code f,
-       pa_state_coverage_code_vw g,
-       pa_operator_age_code i,
-       pa_sex_and_marital_status_code j,
-       pa_vehicle_use_code k,
-       pa_vehicle_performance_code l,
-       pa_penalty_points_code m,
-       pa_body_style_code o,
-       pa_body_size_code p,
-       pa_package_code q,
-       pa_passive_restraint_discount_code r,
-       pa_anti_lock_brakes_discount_code s,
-       pa_defensive_driver_discount_code t
-WHERE  a.line = b.code
-and a.subline = '1'
-and a.subline = c.code
-and a.state = d.code
-and a.trans = e.code
-and a.prog_cd = f.code
-and g.fk_state_id = d.id
-and a.cov_code = g.coverage_code
-and a.age = i.code
-and a.sex_marital = j.code
-and a.vehicle_use = k.code
-and a.vehicle_perf = l.code
-and a.penalty_points = m.code
-and a.body_style = o.code
-and a.body_size = p.code
-and a.package_id = q.code
-and a.pass_restraint  = r.code
-and a.anti_lock = s.code
-and a.def_driver = t.code; 
+       t.id fk_defensive_driver_discount_code
+from pa_stat_stg a join openidl_lob_code b on a.line = b.code
+left join pa_subline_code c on a.subline = c.code
+left join state_code d on a.state = d.code
+left join pa_transaction_code e  on a.trans = e.code
+left join pa_program_code f on a.prog_cd  = f.code
+left join pa_state_coverage_code_vw g on a.cov_code  = g.coverage_code and g.fk_state_id = d.id
+left join pa_operator_age_code i on a.age = i.code
+left join pa_sex_and_marital_status_code j on a.sex_marital = j.code
+left join pa_vehicle_use_code k on a.vehicle_use = k.code
+left join pa_vehicle_performance_code l on a.vehicle_perf = l.code
+left join pa_penalty_points_code m on a.penalty_points = m.code
+left join pa_body_style_code o on a.body_style = o.code
+left join pa_body_size_code p on a.body_size  = p.code
+left join pa_package_code q on a.package_id = q.code
+left join pa_passive_restraint_discount_code r on a.pass_restraint = r.code
+left join pa_anti_lock_brakes_discount_code s on a.anti_lock = s.code
+left join pa_defensive_driver_discount_code t on a.def_driver = t.code
+left join pa_liability_limit_code u on a.lia_lim = u.code and u.fk_coverage_id = g.coverage_id and u.fk_state_id = d.id
+left join pa_private_passenger_drivers_training_good_student_code v on a.driv_train_good_student = v.code
+left join pa_deductible_code w on a.ded_amt = w.code
+left join pa_cause_of_loss_code x on a.col = x.loss_code and x.fk_coverage_code_id = g.coverage_id;
