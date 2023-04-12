52ec2dcae58baccda007acd9302d1f6a3e7429bb
antleypk2 <97132380+antleypk2@users.noreply.github.com>
2023-04-12 15:36:33 -0400

    update files

--- openidl-hds-extraction/pa/auto-coverage/scripts/pa_coverage_ref_tbl.js
+++ openidl-hds-extraction/pa/auto-coverage/scripts/pa_coverage_ref_tbl.js
@@ -1,11 +1,11 @@
 function getRefTable(companyId){
     sql = `
 CREATE TABLE openidl_ep_${companyId}.tmp_pa_coverage_ref as
-SELECT reporting_code, reporting_name 
-FROM openidl_ep_${companyId}.tmp_pa_coverage 
-WHERE reporting_code IS NOT NULL
-GROUP BY reporting_code, reporting_name 
-ORDER BY reporting_code::numeric;
+    SELECT reporting_code, reporting_name 
+    FROM openidl_ep_${companyId}.tmp_pa_coverage 
+    WHERE reporting_code IS NOT NULL
+    GROUP BY reporting_code, reporting_name 
+    ORDER BY reporting_code::numeric;
 
     `
     return sql
