b3d9286c083cc0f67fca3bf976395c6b0855f72a
antleypk2 <97132380+antleypk2@users.noreply.github.com>
2023-04-26 09:44:21 -0400

    fix dates in reduce

--- openidl-hds-extraction/pa/auto-coverage/sql/9997/tmp_pa_coverage_ref_tbl.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE openidl_ep_9997.tmp_pa_coverage_ref as
-    SELECT reporting_code, reporting_name 
-    FROM openidl_ep_9997.tmp_pa_coverage 
-    WHERE reporting_code IS NOT NULL
-    GROUP BY reporting_code, reporting_name 
-    ORDER BY reporting_code::numeric;
-
-    
