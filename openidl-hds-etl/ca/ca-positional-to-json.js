2bf13e028b74f2df8048592a5d21ec99cc2246a4
Mason Wagoner <masonw@Amers-MBP.attlocal.net>
2023-06-05 11:11:33 -0400

    finalized fp, beginning ca

---
+++ openidl-hds-etl/ca/ca-positional-to-json.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const convertToJson =
+  require("../../openidl-etl-statplan-processor/commercial-auto-processor").convertTextRecordsToJson;
+
+
+const config = require("../config/config.json");
+let testPremiumRecordsText = fs.readFileSync(config.commercialAuto.inbound, "utf-8");
+let jsonPremiumRecords = convertToJson(testPremiumRecordsText);
+console.log(jsonPremiumRecords.length)
+
+//let save = {'records': jsonPremiumRecords.slice(0,10)}
+let save = {'records': jsonPremiumRecords}
+
+fs.writeFileSync(config.commercialAuto.outbound, JSON.stringify(save));
